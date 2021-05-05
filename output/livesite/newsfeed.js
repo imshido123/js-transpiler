9e3b43bb9008239b0b357a04607f5b646cc79bed
imshido123 <lujadr@hotmail.com>
2021-05-05 09:42:21 -0500

    New version for output JS

--- output/livesite/newsfeed.js
+++ output/livesite/newsfeed.js
@@ -1,2 +1,2 @@
-//BuildDate:: Wed, May 5, 2021, 9:37 AM CDT
+//BuildDate:: Wed, May 5, 2021, 9:40 AM CDT
 "use strict";var finalResponse={buildInfo:"".concat(new Date().toLocaleString("en-US",dateSettings))};Object.assign(finalResponse,body);
---
+++ output/target/alerta.js
@@ -0,0 +1,2 @@
+//BuildDate:: Wed, May 5, 2021, 9:40 AM CDT
+"use strict";
